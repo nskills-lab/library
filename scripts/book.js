db08827cee62aa6ef29cb0a83bbaddbfa3e97aef
Nina Rey <reynina.w@gmail.com>
2022-07-05 11:29:17 -0600

    Move Book into a class

---
+++ scripts/book.js
@@ -0,0 +1,9 @@
+export class Book {
+  constructor(id, title, author, pages, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
